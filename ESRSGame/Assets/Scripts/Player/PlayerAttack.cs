260a6b69808aeb7d3b593b207617b877161f8636
lightblood7 <jfenton1997@gmail.com>
2021-02-13 20:22:08 +0000

    V0.0.1 Unity Init
    + player Movement
    + player rotation
    + test room
    + basic camera

---
+++ ESRSGame/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAttack : MonoBehaviour
+    {
+
+        [SerializeField] private float horizontal;
+        [SerializeField] private float vertical;
+
+        private void FixedUpdate()
+        {
+            horizontal = Input.GetAxis("Horizontal_LeftRight");
+            vertical = Input.GetAxis("Vertical_DownUp");
+            RotateToDirection();
+        
+
+        }
+
+
+
+        private void RotateToDirection()
+        {
+            if(horizontal != 0)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, horizontal > 0 ? Quaternion.Euler(0f,90f,0f) : Quaternion.Euler(0f,270f,0f), 0.1f);
+            }
+
+            if (vertical == 0) return;
+            transform.rotation = Quaternion.Lerp(transform.rotation, vertical > 0 ? Quaternion.Euler(0f,0f,0f) : Quaternion.Euler(0f,180f,0f), 0.1f);
+        }
+    }
+}
+
---
+++ ESRSGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 10f;
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            float horizontal = Input.GetAxis("Horizontal_AD");
+            float vertical = Input.GetAxis("Vertical_WS");
+            Vector3 move = new Vector3(horizontal, 0, vertical);
+        
+            _rigidbody.velocity = move * speed;
+        }
+    }
+}
