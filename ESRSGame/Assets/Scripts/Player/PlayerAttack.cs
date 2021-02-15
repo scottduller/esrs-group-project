4ced7915e8df461412c4e413acdd00a576ac574a
lightblood7 <jfenton1997@gmail.com>
2021-02-15 15:04:26 +0000

    V0.012
    ~ changed controls. look at mouse
    + utils class

--- ESRSGame/Assets/Scripts/Player/PlayerAttack.cs
+++ ESRSGame/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Player
 {
@@ -8,26 +9,9 @@ namespace Player
         [SerializeField] private float horizontal;
         [SerializeField] private float vertical;
 
-        private void FixedUpdate()
+        private void Start()
         {
-            horizontal = Input.GetAxis("Horizontal_LeftRight");
-            vertical = Input.GetAxis("Vertical_DownUp");
-            RotateToDirection();
-        
-
-        }
-
-
-
-        private void RotateToDirection()
-        {
-            if(horizontal != 0)
-            {
-                transform.rotation = Quaternion.Lerp(transform.rotation, horizontal > 0 ? Quaternion.Euler(0f,90f,0f) : Quaternion.Euler(0f,270f,0f), 0.1f);
-            }
-
-            if (vertical == 0) return;
-            transform.rotation = Quaternion.Lerp(transform.rotation, vertical > 0 ? Quaternion.Euler(0f,0f,0f) : Quaternion.Euler(0f,180f,0f), 0.1f);
+            
         }
     }
 }
---
+++ ESRSGame/Assets/Scripts/UtilsClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilsClass
+{
+    private static Camera mainCamera ;
+    
+    public static Vector3 GetMouseWorldPosition(){
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+        Ray castPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            return hit.point;
+        }
+
+        return new Vector3(0, 0, 0);
+
+
+    }
+
+}
