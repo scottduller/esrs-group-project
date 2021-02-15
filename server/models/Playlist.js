4df60d3c6c89630c169151c71e737eb94a6d5d81
Scott Duller <scottduller@gmail.com>
2021-02-15 17:35:36 +0000

    routes commplete

--- server/models/Playlist.js
+++ server/models/Playlist.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var levelSchema = new mongoose.Schema({
+var playlistSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
